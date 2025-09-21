698e53a92820f13004bd4368830f0961d728fd7b
Raphaël Franchet <raphael.franchet@ametys.org>
2025-09-21 14:19:05 +0200

    First implementation for Conan Scenarios #1

--- src/fr/rafdulaf/scenarios4monolith/general/CompanionSynchronizableCollection.java
+++ src/fr/rafdulaf/scenarios4monolith/general/CompanionSynchronizableCollection.java
@@ -1,26 +1,124 @@
 package fr.rafdulaf.scenarios4monolith.general;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
 import org.slf4j.Logger;
 
+import org.ametys.cms.repository.Content;
+import org.ametys.cms.repository.ModifiableContent;
+import org.ametys.cms.repository.WorkflowAwareContent;
+import org.ametys.core.util.JSONUtils;
 import org.ametys.plugins.contentio.synchronize.impl.AbstractDefaultSynchronizableContentsCollection;
+import org.ametys.plugins.repository.metadata.MultilingualString;
+import org.ametys.runtime.model.View;
+
+import com.opensymphony.workflow.WorkflowException;
 
 /**
  * Synchronize with the Companion data
  */
 public class CompanionSynchronizableCollection extends AbstractDefaultSynchronizableContentsCollection
 {
+    private JSONUtils _jsonUtils;
+
+    @Override
+        public void service(ServiceManager manager) throws ServiceException
+        {
+            super.service(manager);
+            _jsonUtils = (JSONUtils) manager.lookup(JSONUtils.ROLE);
+        }
+    
     public String getIdField()
     {
-        return "id";
+        return "identifier";
     }
     
     @Override
     protected Map<String, Map<String, Object>> internalSearch(Map<String, Object> searchParameters, int offset, int limit, List<Object> sort, Logger logger)
     {
-        // TODO Auto-generated method stub
-        return null;
+        // TODO
+        return Map.of(
+            "carbu", Map.of("identifier", "carbu", "title", "{\"fr\": \"Carburant\", \"en\": \"Fuel\"}", "type", "Essence")
+        );
+    }
+    
+    @Override
+    public ModifiableContent getContent(String lang, String idValue, boolean forceStrictCheck)
+    {
+        // Ignore language for multilingual reference table
+        return super.getContent(null, idValue, forceStrictCheck);
+    }
+    
+    @Override
+    protected ModifiableContent createContentAction(String contentType, String workflowName, int initialActionId, String lang, String contentTitle, Logger logger)
+    {
+        try
+        {
+            logger.info("Creating content '{}' with the content type '{}' for language {}", contentTitle, getContentType(), lang);
+            
+            Map<String, Object> inputs = _getAdditionalInputsForContentCreation();
+            Map<String, Object> result;
+            
+            if ("-".equals(lang))
+            {
+                // The title is a JSON object, we use it as is
+                @SuppressWarnings({"unchecked"})
+                Map<String, String> titleVariants = (Map) _jsonUtils.convertJsonToMap(contentTitle);
+                String desiredContentName = _contentPrefix + "-" + titleVariants.get("en") + "-" + lang;
+                result = _contentWorkflowHelper.createContent(
+                        workflowName,
+                        initialActionId,
+                        desiredContentName,
+                        titleVariants,
+                        new String[] {contentType},
+                        null,
+                        inputs);
+            }
+            else
+            {
+                String desiredContentName = _contentPrefix + "-" + contentTitle + "-" + lang;
+                result = _contentWorkflowHelper.createContent(
+                        workflowName,
+                        initialActionId,
+                        desiredContentName,
+                        contentTitle,
+                        new String[] {contentType},
+                        null,
+                        lang,
+                        inputs);
+            }
+            
+            return (ModifiableContent) result.get(Content.class.getName());
+        }
+        catch (WorkflowException e)
+        {
+            _nbError++;
+            logger.error("Failed to initialize workflow for content {} and language {}", contentTitle, lang, e);
+            return null;
+        }
+    }
+    
+    @Override
+    protected boolean _editContent(WorkflowAwareContent content, Optional<View> view, Map<String, Object> values, Map<String, Object> additionalParameters, boolean create, Set<String> notSynchronizedContentIds, Logger logger) throws WorkflowException
+    {
+        if (values.containsKey("title")
+            && "multilingual-string".equals(content.getDefinition("title").getType().getId()))
+        {
+            MultilingualString title = new MultilingualString();
+            
+            @SuppressWarnings("unchecked")
+            Map<String, String> titleVariants = (Map) _jsonUtils.convertJsonToMap((String) values.get("title"));
+            titleVariants.entrySet().stream().forEach(e -> title.add(Locale.forLanguageTag(e.getKey()), e.getValue()));
+            
+            values.put("title", title);
+        }
+        
+        return super._editContent(content, view, values, additionalParameters, create, notSynchronizedContentIds, logger);
     }
 }
---
+++ src/fr/rafdulaf/scenarios4monolith/general/SynchronizableContentsCollectionDAO.java
@@ -0,0 +1,68 @@
+package fr.rafdulaf.scenarios4monolith.general;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+
+import org.ametys.cms.contenttype.ContentType;
+import org.ametys.cms.contenttype.ContentTypeExtensionPoint;
+import org.ametys.core.ui.Callable;
+import org.ametys.runtime.i18n.I18nizableText;
+
+public class SynchronizableContentsCollectionDAO extends org.ametys.plugins.contentio.synchronize.SynchronizableContentsCollectionDAO
+{
+    private ContentTypeExtensionPoint _contentTypeEP;
+
+    
+    @Override
+    public void service(ServiceManager smanager) throws ServiceException
+    {
+        super.service(smanager);
+        _contentTypeEP = (ContentTypeExtensionPoint) smanager.lookup(ContentTypeExtensionPoint.ROLE);
+    }
+    
+    @Override
+    @Callable
+    public Map<String, Object> getEditionConfiguration() throws Exception
+    {
+        Map<String, Object> result = super.getEditionConfiguration();
+        
+        // CONTENT TYPES
+        result.put(
+            "contentTypes",
+            _transformToJSONEnumerator(
+                _contentTypeEP.getExtensionsIds().stream().map(_contentTypeEP::getExtension).filter(this::_isValidContentType),
+                ContentType::getId,
+                ContentType::getLabel
+            )
+        );
+        
+        @SuppressWarnings("unchecked")
+        List<Map<String, Object>> languages = new ArrayList<>((List<Map<String, Object>>) result.get("languages"));
+        languages.add(Map.of("value", "-", "label", new I18nizableText("-")));
+        result.put("languages", languages);
+        
+        return result;
+    }
+    
+    private <T> List<Map<String, Object>> _transformToJSONEnumerator(Stream<T> values, Function<T, String> valueFunction, Function<T, I18nizableText> labelFunction)
+    {
+        return values.map(value ->
+                Map.of(
+                    "value", valueFunction.apply(value),
+                    "label", labelFunction.apply(value)
+                )
+            )
+            .toList();
+    }
+    
+    private boolean _isValidContentType (ContentType cType)
+    {
+        return !cType.isAbstract() && !cType.isMixin();
+    }
+}
